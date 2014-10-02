74eb23e5d823c58d9f9a245e15b9f192b991e18b
Adam <adamwatters11@gmail.com>
2014-10-02 15:16:55 -0400

    derp

--- main.jsx
+++ main.jsx
@@ -3,24 +3,73 @@
 var Square = React.createClass({
 
 	getInitialState: function() {
-		return {hidden: true};
+		return {hidden: true, flagged: false, zeroArray:[]};
 	},
 
-	handleClick: function() {
-		if (this.state.hidden) {
-			this.setState({hidden: false})
+	flagCheck: function() {
+		if(!this.state.flagged) {
+			this.props.flagsOutChange(1);
+		} else {
+			this.props.flagsOutChange(-1);
+		};
+		if (this.props.mine && !this.state.flagged) {
+				this.props.correctFlagPlaced();
+			} else if (this.props.mine && this.state.flagged) {
+				this.props.correctFlagRemoved();
+			}
+	},
+
+	reveal: function() {
+		this.setState({hidden: false});
+		if(this.props.near === 0 && this.props.mine != true){
+			this.props.updateZeroArray(this.props.x,this.props.y);
 		};
 	},
 
-	getPosition: function() {
+	handleClick: function() {
+
+		if (this.props.flagMode && this.state.hidden){
+			this.flagCheck();
+			this.setState({flagged: !this.state.flagged});
+		} else {
+			if (this.state.hidden && !this.state.flagged) {
+				this.reveal();
+			};
+			if (this.props.mine) {
+				this.props.setGameState('lose');
+			};
+		};
+	},
+
+	getStyle: function() {
+
+		var color = ''
+		var text = ''
+
+
+		if (this.state.flagged) {
+			color = 'red';
+			text = 'white';
+		} else if (this.state.hidden) {
+			color = 'grey';
+			text = 'black';
+		} else {
+			color = 'orange';
+			text = 'black';
+		};
+
 		return {
 			left: this.props.x * 50,
-			top: this.props.y * 50
+			top: this.props.y * 50,
+			backgroundColor: color,
+			color: text
 		};
 	},
 
 	getDisplay: function() {
-		if (this.state.hidden) {
+		if (this.state.flagged) {
+			return "F"
+		}else if (this.state.hidden) {
 			return " ";
 		} else {
 			if (this.props.mine) {
@@ -28,14 +77,31 @@ var Square = React.createClass({
 			} else if (this.props.near > 0){
 				return this.props.near;
 			} else {
-				return "0"
+				return " "
 			};
 		};
 	},
 
+	componentDidUpdate: function() {
+		this.props.zeroArray.forEach(function(zero) {
+			if(
+				(this.state.hidden === true) &&
+					(
+						this.props.x === (zero.x + 1) && (this.props.y >= (zero.y - 1) && this.props.y <= (zero.y + 1)) ||
+						this.props.x === (zero.x - 1) && (this.props.y >= (zero.y + 1) && this.props.y <= (zero.y + 1)) ||
+						this.props.y === (zero.y + 1) && this.props.x === zero.x ||
+						this.props.y === (zero.y - 1) && this.props.x === zero.x
+					) 
+			) {
+				this.reveal();
+			}
+
+		}.bind(this));
+	},
+
 	render: function() {
 		return (
-			<div className='square' style={this.getPosition()} onClick={this.handleClick}>
+			<div className='square' style={this.getStyle()} onClick={this.handleClick}>
 				{this.getDisplay()}
 			</div>
 		);
@@ -46,18 +112,34 @@ var Board = React.createClass({
 
 	getSize: function() {
 		return {
-			height: this.props.mapSize.height * 50,
-			width: this.props.mapSize.width * 50
+			height: this.props.mapMeta.height * 50,
+			width: this.props.mapMeta.width * 50
 		};
 	},
 
 	render: function() {
 		var squares = [];
 		var key = 0;
-		this.props.map.forEach(function(location){
-			squares.push(<Square key={key} x={location.x} y={location.y} mine={location.mine} near={location.near} />)
+		this.props.map.forEach(function(cell){
+			squares.push(<Square 
+							key={key} 
+							x={cell.x} 
+							y={cell.y}
+							mine={cell.mine} 
+							near={cell.near} 
+							setGameState={this.props.setGameState}
+							mines={this.props.mapMeta.mines}
+							minesRemaining={this.props.minesRemaining} 
+							correctFlagPlaced={this.props.correctFlagPlaced}
+							correctFlagRemoved={this.props.correctFlagRemoved}
+							flagsOut={this.props.flagsOut}
+							flagsOutChange={this.props.flagsOutChange}
+							updateZeroArray={this.props.updateZeroArray}
+							zeroArray={this.props.zeroArray}
+							flagMode={this.props.flagMode}/>
+						);
 			key ++;
-		});
+		}.bind(this));
 		return (
 			<div className='board' style={this.getSize()}>
 				{squares}	
@@ -67,37 +149,121 @@ var Board = React.createClass({
 });
 
 var Game = React.createClass({
+	getInitialState: function() {
+		return {
+			zeroArray: [],
+			gameState: 'active', 
+			flagMode: false, 
+			minesRemaining: this.props.mapMeta.mines, 
+			flagsOut: 0
+		};
+	},
+
+	updateZeroArray: function(x,y) {
+		
+		this.setState({zeroArray: this.state.zeroArray.concat([{x: x, y: y}])});
+	
+	},
+
+	setGameState: function(stateUpdate) {        // 'active', 'win', or 'lose'
+		this.setState({gameState: stateUpdate});
+	},
+
+	flagsOutChange: function(num) {
+		this.setState({flagsOut: this.state.flagsOut + num});
+	},
+
+	correctFlagPlaced: function() {
+		this.setState({minesRemaining: this.state.minesRemaining - 1});
+	},
+
+	correctFlagRemoved: function() {
+		this.setState({minesRemaining: this.state.minesRemaining + 1});
+	},
+
+	handleKeyDown: function (event) {
+		if (event.altKey) {
+			event.preventDefault();
+			this.setState({flagMode: !this.state.flagMode});
+		};
+	},
+
+	componentDidMount: function () {
+		window.addEventListener('keydown', this.handleKeyDown);
+	},
+
+	componentWillUnmount: function () {
+		window.removeEventListener('keydown', this.handleKeyDown);
+	},
+
+	componentDidUpdate: function() {
+
+		if(this.props.mapMeta.mines === this.state.flagsOut && this.state.minesRemaining === 0 && this.state.gameState != 'win'){
+			this.setGameState('win');
+		}
+	},
+
 	render: function() {
-		return (
-			<div className='game'>
-				<Board mapSize={this.props.mapSize} map={this.props.map} />
-			</div>
-		);
+
+		if (this.state.gameState === 'active'){
+			return (
+				<div className='game'>
+					<Board 
+						mapMeta={this.props.mapMeta} 
+						map={this.props.map}
+						setGameState={this.setGameState}
+						minesRemaining={this.state.minesRemaining} 
+						correctFlagPlaced={this.correctFlagPlaced}
+						correctFlagRemoved={this.correctFlagRemoved}
+						flagsOut={this.state.flagsOut}
+						flagsOutChange={this.flagsOutChange}
+						updateZeroArray={this.updateZeroArray}
+						zeroArray={this.state.zeroArray}
+						flagMode={this.state.flagMode}/>
+					<h1> press ALT to toggle flag mode </h1>
+				</div>
+			);
+		} else if(this.state.gameState === 'lose') {
+			return (
+				<div className='game'>
+					<Board 
+						mapMeta={this.props.mapMeta} 
+						map={this.props.map}
+						setGameState={this.setGameState}
+						minesRemaining={this.state.minesRemaining} 
+						correctFlagPlaced={this.correctFlagPlaced}
+						correctFlagRemoved={this.correctFlagRemoved}
+						flagsOut={this.state.flagsOut}
+						flagsOutChange={this.flagsOutChange}
+						updateZeroArray={this.updateZeroArray}
+						zeroArray={this.state.zeroArray}
+						flagMode={this.state.flagMode}/>
+					<h1> YOU LOSE </h1>
+				</div>
+			);
+		} else if(this.state.gameState === 'win'){
+			return (
+				<div className='game'>
+					<Board 
+						mapMeta={this.props.mapMeta} 
+						map={this.props.map}
+						setGameState={this.setGameState}
+						minesRemaining={this.state.minesRemaining} 
+						correctFlagPlaced={this.correctFlagPlaced}
+						correctFlagRemoved={this.correctFlagRemoved}
+						flagsOut={this.state.flagsOut}
+						flagsOutChange={this.flagsOutChange}
+						updateZeroArray={this.updateZeroArray}
+						zeroArray={this.state.zeroArray}
+						flagMode={this.state.flagMode}/>
+					<h1> YOU WIN </h1>
+				</div>
+			);
+		}
 	}
 });
 
-var SIZE = {height: 4, width: 4};
-
-var MAP = [
-	{x: 0, y: 0, mine: false, near: 0},
-	{x: 1, y: 0, mine: false, near: 0},
-	{x: 2, y: 0, mine: false, near: 0},
-	{x: 3, y: 0, mine: false, near: 0},
-	{x: 0, y: 1, mine: false, near: 1},
-	{x: 1, y: 1, mine: false, near: 1},
-	{x: 2, y: 1, mine: false, near: 1},
-	{x: 3, y: 1, mine: false, near: 0},
-	{x: 0, y: 2, mine: false, near: 2},
-	{x: 1, y: 2, mine: true, near: 1},
-	{x: 2, y: 2, mine: false, near: 3},
-	{x: 3, y: 2, mine: false, near: 1},
-	{x: 0, y: 3, mine: false, near: 2},
-	{x: 1, y: 3, mine: true, near: 1},
-	{x: 2, y: 3, mine: false, near: 3},
-	{x: 3, y: 3, mine: true, near: 0}
-];
-
 React.renderComponent(
-	<Game mapSize={SIZE} map={MAP}/>,
+	<Game mapMeta={META} map={MAP} />,
 	document.getElementById('wrapper')
 );
---
+++ mapMaker.js
@@ -0,0 +1,31 @@
+
+var META = {height:12, width:12, mines:0}
+var MAP = [];
+var mineMaker;
+
+for (var i = 0; i < META.width; i++){
+	for (var j = 0; j < META.height; j++){
+		if (Math.random()>.85) {
+			mineMaker = true;
+			META.mines ++;
+		} else {
+			mineMaker = false;
+		}
+		MAP.push({x: i, y: j, mine: mineMaker, near: 0})
+	}
+}
+
+MAP.forEach(function(cellOne){
+	if (cellOne.mine === true){
+		MAP.forEach(function(cellTwo){
+			if (
+				cellTwo.x === (cellOne.x + 1) && (cellTwo.y >= (cellOne.y - 1) && cellTwo.y <= (cellOne.y + 1)) ||
+				cellTwo.x === (cellOne.x - 1) && (cellTwo.y >= (cellOne.y - 1) && cellTwo.y <= (cellOne.y + 1)) ||
+				cellTwo.y === (cellOne.y + 1) && cellTwo.x === cellOne.x ||
+				cellTwo.y === (cellOne.y - 1) && cellTwo.x === cellOne.x
+			){
+				cellTwo.near ++;
+			}
+		});
+	};
+});
